1e93c5cbb16c7fa0cfcf0bd383a89a3d08c56712
eswarlearn <me4717187@gmail.com>
2025-06-21 22:02:58 +0530

    twilio sms verification

---
+++ go-sms-verify/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/eswarlearn/go-sms-verify/api"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	//initialize config
+	app := api.Config{Router: router}
+
+	app.Routes()
+
+	router.Run(":8081") // Start the server on port 8081
+
+}
